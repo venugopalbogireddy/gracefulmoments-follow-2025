73da850e9d5cd0dffeb2a42663ad7c4b25884adf
Venugopl Bogireddy <venuiitms@gmail.com>
2025-11-30 16:12:50 -0500

    Initial Commit

---
+++ GracefulMoments/GracefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GracefulMomentsApp.swift
+//  GracefulMoments
+//
+//  Created by Venugopal Bogireddy on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GracefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
